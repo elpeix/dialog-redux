47a663a64d5a0998c0481eb9924faa82b0a89f16
Francesc Requesens <francesc.requesens@gmail.com>
2022-09-20 19:32:23 +0200

    Removed counter

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import dialogsReducer from '../features/dialogCanvas/dialogsSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     dialogs: dialogsReducer
   },
   middleware: (getDefaultMiddleware) =>
